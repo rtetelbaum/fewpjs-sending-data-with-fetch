34d2b0ef0d64f7d9070a22bb1bf12f49a6d5452d
rtetelbaum <rtetelbaum@mailbox.org>
2020-11-19 19:45:44 -0500

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+	const userData = {
+		name: name,
+		email: email
+	}
+
+	const body = document.querySelector("body")
+
+	return fetch(`http://localhost:3000/users`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		},
+		body: JSON.stringify(userData),
+		})
+		.then(response => response.json())
+		.then(userData => {
+			console.log(userData)
+			const userId = userData.id
+			body.append(userId)
+		})
+		.catch(error => {
+			body.append(error.message)
+		})
+}
+
+submitData("roman", "roman@roman.com")
